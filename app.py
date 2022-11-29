bc987e8778e5c3c90a6ff128bc565bbf76943138
emirhan <72984140+lareithen@users.noreply.github.com>
2022-11-29 23:46:04 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,39 +1,48 @@
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
+from tinydb import TinyDB
 import logging
 
 log = logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)
+db = TinyDB('db.json')
 app = Flask(__name__)
 app.config['DEBUG'] = True
 socketio = SocketIO(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html', user=request.remote_addr)
+    messages = db.all()
+    return render_template('index.html', user=request.remote_addr, messages=messages)
 
 @socketio.on('message')
 def handler(data):
+    data = {'data': f"{dict(data)['user']}: {dict(data)['data']}"}
     print(data)
+    db.insert(data)
     emit(
         'response',
-        {'data': f"{dict(data)['user']}: {dict(data)['data']}"}, 
+        data, 
         broadcast=True
     )
 
 @socketio.event
 def connect():
+    data = {'data': f'{request.remote_addr} connected.'}
+    db.insert(data)
     emit(
         'response', 
-        {'data': f'{request.remote_addr} connected.'},
+        data,
         broadcast=True
     )
 
 @socketio.event
 def disconnect():
+    data = {'data': f'{request.remote_addr} disconnected.'}
+    db.insert(data)
     emit(
         'response', 
-        {'data': f'{request.remote_addr} disconnected.'}, 
+        {'data': data}, 
         broadcast=True
     )
 
